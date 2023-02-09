3e6ad32c13d6f25610e8fddc6a6e7a910152bc78
edward-e-daisey <121644982+edward-e-daisey@users.noreply.github.com>
2023-02-09 15:57:24 -0600

    Add files via upload

---
+++ Project2/main.tf
@@ -0,0 +1,66 @@
+locals {
+  vpc_id           = "vpc-03beadf92dce8e844"
+  subnet_id        = "subnet-09fec4605fdd2b6d3"
+  ssh_user         = "ubuntu"
+  key_name         = "devops"
+  private_key_path = "~/Downloads/devops.pem"
+}
+
+provider "aws" {
+  access_key = "TOPSECRET"
+  secret_key = "TOPSECRET"
+  region     = "us-east-1"
+}
+
+resource "aws_security_group" "nginx" {
+  name   = "nginx_access"
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                         = "ami-0557a15b87f6559cf"
+  subnet_id                   = "subnet-09fec4605fdd2b6d3"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.nginx.id]
+  key_name                    = local.key_name
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} nginx.yaml"
+  }
+}
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip
+}
